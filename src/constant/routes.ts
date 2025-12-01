b39cf27094ba197b67aba332af543f5f1d63a61e
datvpse171622 <datvpse171622@fpt.edu.vn>
2025-12-01 19:37:34 +0700

    chore: translate path title to vietnamese

--- src/constant/routes.ts
+++ src/constant/routes.ts
@@ -2,21 +2,21 @@
 
 export const ROUTES = {
     // PUBLIC
-    HOME: { path: '/', label: 'Home', public: true, getPath: () => '/' },
-    ABOUT: { path: '/about', label: 'About', public: true, getPath: () => '/about' },
-    CONTACT: { path: '/contact-us', label: 'About', public: true, getPath: () => '/contact-us' },
-    PRICING: { path: '/pricing', label: 'Pricing', public: true, getPath: () => '/pricing' },
+    HOME: { path: '/', label: 'Trang chủ', public: true, getPath: () => '/' },
+    ABOUT: { path: '/about', label: 'Về chúng tôi', public: true, getPath: () => '/about' },
+    CONTACT: { path: '/contact-us', label: 'Liên hệ', public: true, getPath: () => '/contact-us' },
+    PRICING: { path: '/pricing', label: 'Bảng giá', public: true, getPath: () => '/pricing' },
 
-    POLICY: { path: '/policy', label: 'Policy', public: true, getPath: () => '/policy' },
-    TOS: { path: '/terms-of-service', label: 'Terms of Service', public: true, getPath: () => '/terms-of-service' },
+    POLICY: { path: '/policy', label: 'Chính sách', public: true, getPath: () => '/policy' },
+    TOS: { path: '/terms-of-service', label: 'Điều khoản dịch vụ', public: true, getPath: () => '/terms-of-service' },
 
 
-    USER_DETAIL: { path: '/user/:id', getPath: (id: string) => `/user/${id}`, label: 'User', public: true },
+    USER_DETAIL: { path: '/user/:id', getPath: (id: string) => `/user/${id}`, label: 'Người dùng', public: true },
 
 
     CHECKOUT: {
         path: '/checkout/:id',
-        label: 'Checkout',
+        label: 'Thanh toán',
         public: true,
         getPath: (id: string) => `/checkout/${id}`
     },
@@ -24,35 +24,38 @@ export const ROUTES = {
 
     // AUTH
     DASHBOARD: {
-        path: '/dashboard', label: 'Dashboard', authOnly: true,
+        path: '/dashboard', label: 'Bảng điều khiển', authOnly: true,
         getPath: () => '/dashboard',
         child: {
-            USER_MANAGEMENT: { path: 'user-list', label: 'User List', getPath: () => '/dashboard/user-list' },
-            PERMISSION_MANAGEMENT: { path: 'permissions', label: 'Permission Management', getPath: () => '/dashboard/permissions' },
-            MY_TRANSACTION: { path: 'transaction/my-transaction', label: 'My Transaction', getPath: () => '/dashboard/transaction/my-transaction' },
-            TRANSACTION: { path: 'transactions', label: 'Transactions', getPath: () => '/dashboard/transactions' },
+            USER_MANAGEMENT: { path: 'user-list', label: 'Quản lý người dùng', getPath: () => '/dashboard/user-list' },
+            PERMISSION_MANAGEMENT: { path: 'permissions', label: 'Quản lý quyền', getPath: () => '/dashboard/permissions' },
+            MY_TRANSACTION: { path: 'transaction/my-transaction', label: 'Giao dịch của tôi', getPath: () => '/dashboard/transaction/my-transaction' },
+            TRANSACTION: { path: 'transactions', label: 'Quản lý giao dịch', getPath: () => '/dashboard/transactions' },
 
 
             // Notification
             NOTIFICATION_MANAGEMENT: {
-                path: 'notification/notification-management', label: 'Notification Management',
+                path: 'notification/notification-management', label: 'Quản lý thông báo',
                 getPath: () => '/dashboard/notification/notification-management',
 
             },
+            CLUB_QUEUE: {
+                path: 'clubs/queue', label: 'Club chờ duyệt', getPath: () => '/dashboard/clubs/queue'
+            },
 
             // Ticket
-            TICKET_MANAGEMENT: { path: 'ticket/ticket-management', label: 'Ticket Management', getPath: () => '/dashboard/ticket/ticket-management' },
-            MY_TICKET: { path: 'ticket/my-ticket', label: 'My Ticket', getPath: () => '/dashboard/ticket/my-ticket' },
+            TICKET_MANAGEMENT: { path: 'ticket/ticket-management', label: 'Quản lý Ticket', getPath: () => '/dashboard/ticket/ticket-management' },
+            MY_TICKET: { path: 'ticket/my-ticket', label: 'Ticket của tôi', getPath: () => '/dashboard/ticket/my-ticket' },
 
             // Settings
-            ACCOUNT_SETTINGS: { path: 'settings', label: 'Settings', getPath: () => '/dashboard/settings' },
+            ACCOUNT_SETTINGS: { path: 'settings', label: 'Cài đặt', getPath: () => '/dashboard/settings' },
             // SECURITY_SETTINGS: { path: 'settings/security', label: 'Security' },
             // NOTIFICATION_SETTINGS: { path: 'settings/notification', label: 'Notification' },
 
-            LOG: { path: 'log', label: 'Log', getPath: () => '/dashboard/log' },
+            LOG: { path: 'log', label: 'Nhật ký', getPath: () => '/dashboard/log' },
         }
     },
 
     // NOT FOUND
-    NOT_FOUND: { path: '/404', label: 'Not Found', public: true, getPath: () => '/404' },
+    NOT_FOUND: { path: '/404', label: 'Không tìm thấy', public: true, getPath: () => '/404' },
 };
